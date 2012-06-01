322a3fb899f2ae19f8ce39e397311781f705d4bb
Jorge Pardiñas <jorge.parbros@crowdint.com>
2012-06-01 01:27:37 -0500

    agreggando initializar

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,9 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],
+    :aws_secret_access_key  => ENV['AWS_SECRET_KEY'],
+    :region                 => 'eu-west-1'
+  }
+  config.fog_directory  = 'conteociudadano'
+end
---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,4 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, 'fP4o5frRF4uAowJX3Uz5TQ', '04ecvOhlObD0wGEb3lcsPGGXBTjnQa8nyVue4waG4'
+  provider :facebook, '419620151411274', '62a84e25acab5c0da30e918d2b1d57fa'
+end
