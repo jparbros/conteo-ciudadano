28d1d642516aa536d4edb3316825ad71186e3c79
Jorge Pardiñas <jorge.parbros@crowdint.com>
2012-06-22 14:28:42 -0500

    desabilitando el SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Conteo::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  # config.force_ssl = true
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
@@ -43,7 +43,7 @@ Conteo::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "https://assets%d.conteo-ciudadano.org"
+  config.action_controller.asset_host = "http://assets%d.conteo-ciudadano.org"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
