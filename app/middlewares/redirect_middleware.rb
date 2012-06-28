24ea0234cf160b63ce90b5f03fbdf703e6effe83
Jorge Pardiñas <jorge.parbros@crowdint.com>
2012-06-28 17:04:01 -0500

    debugeando el middleware

--- app/middlewares/redirect_middleware.rb
+++ app/middlewares/redirect_middleware.rb
@@ -8,6 +8,12 @@ class RedirectMiddleware
 
     request = Rack::Request.new(env)
 
+    Rails.logger.info "======================================"
+    Rails.logger.info "request.host => #{request.host}"
+    Rails.logger.info "ENV['ADMIN_URL'] => #{ENV['ADMIN_URL']}"
+    Rails.logger.info " 1st id => #{request.host.match(/#{ENV['ADMIN_URL']}/ ) && !request.host.match(/#{ENV['ADMIN_URL']}\/admin.*/)}"
+    Rails.logger.info " 2nd id => #{!request.host.match /#{ENV['ADMIN_URL']}/ && request.host.end_with?('/admin')}"
+    Rails.logger.info "======================================"
     if request.host.match(/#{ENV['ADMIN_URL']}/ ) && !request.host.match(/#{ENV['ADMIN_URL']}\/admin.*/)
       [301, {"Location" => "https://#{ENV['ADMIN_URL']}/admin"}, self]
     elsif !request.host.match /#{ENV['ADMIN_URL']}/ && request.host.end_with?('/admin')
