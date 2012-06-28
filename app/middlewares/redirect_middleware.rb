613cf74d583e4cae68a6fe4cf07d6e683d61dcd6
Jorge Pardiñas <jorge.parbros@crowdint.com>
2012-06-28 17:25:46 -0500

    debugeando el middleware

--- app/middlewares/redirect_middleware.rb
+++ app/middlewares/redirect_middleware.rb
@@ -11,12 +11,12 @@ class RedirectMiddleware
     Rails.logger.info "======================================"
     Rails.logger.info "request.host => #{request.host}"
     Rails.logger.info "ENV['ADMIN_URL'] => #{ENV['ADMIN_URL']}"
-    Rails.logger.info " 1st id => #{request.host.match(/#{ENV['ADMIN_URL']}/ ) && !request.host.match(/#{ENV['ADMIN_URL']}\/admin.*/)}"
-    Rails.logger.info " 2nd id => #{!request.host.match /#{ENV['ADMIN_URL']}/ && request.host.end_with?('/admin')}"
+    Rails.logger.info " 1st id => #{request.host.match(/#{ENV['ADMIN_URL']}/ ) && !request.uri.match(/#{ENV['ADMIN_URL']}\/admin.*/)}"
+    Rails.logger.info " 2nd id => #{!request.host.match(/#{ENV['ADMIN_URL']}/ )&& request.uri.end_with?('/admin')}"
     Rails.logger.info "======================================"
-    if request.host.match(/#{ENV['ADMIN_URL']}/ ) && !request.host.match(/#{ENV['ADMIN_URL']}\/admin.*/)
+    if request.host.match(/#{ENV['ADMIN_URL']}/ ) && !request.uri.match(/#{ENV['ADMIN_URL']}\/admin.*/)
       [301, {"Location" => "https://#{ENV['ADMIN_URL']}/admin"}, self]
-    elsif !request.host.match /#{ENV['ADMIN_URL']}/ && request.host.end_with?('/admin')
+    elsif !request.host.match /#{ENV['ADMIN_URL']}/ && request.uri.end_with?('/admin')
       [404, {}, self]
     else
       @app.call(env)
