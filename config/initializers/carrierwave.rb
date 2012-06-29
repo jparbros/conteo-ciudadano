bcd42fb00f9d96ff36211d12034e1e6f275f3f1e
Jorge Pardiñas <jorge.parbros@crowdint.com>
2012-06-29 18:15:49 -0500

    agregando el bucket y el directorio

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,6 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],
     :aws_secret_access_key  => ENV['AWS_SECRET_KEY'],
-    :region                 => 'eu-west-1'
   }
-  config.fog_directory  = 'stage'
-  config.s3_bucket = "conteociudadano"
+  config.fog_directory  = 'conteociudadanostage'
 end
